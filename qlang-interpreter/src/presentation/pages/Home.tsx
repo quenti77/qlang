dc9564f7c8f2f68c7c891225126c0889d4c0a1a1
Quentin Ysambert <quentin.ysambert@gmail.com>
2024-06-08 15:21:24 +0200

    :art: change grid for the display interface

--- qlang-interpreter/src/presentation/pages/Home.tsx
+++ qlang-interpreter/src/presentation/pages/Home.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
     return (
         <div className="m-2 grid grid-rows-3 grid-cols-4 gap-2 h-[calc(100vh-4rem)]">
-            <div className="hidden lg:block lg:row-span-2 col-span-2 xl:col-span-1">
+            <div className="hidden lg:block lg:row-start-1 lg:row-span-3 lg:col-span-2 xl:col-span-1">
                 <HelpTile />
             </div>
             <div className="row-span-2 col-span-4 lg:col-span-2 xl:col-span-3">
@@ -32,7 +32,7 @@ export default function Home() {
                     onExecute={handleExecute}
                 />
             </div>
-            <div className="row-start-3 row-span-1 col-span-4">
+            <div className="row-start-3 row-span-1 col-span-4 lg:col-span-2 xl:col-span-3">
                 <OutputTile output={output} />
             </div>
         </div>
